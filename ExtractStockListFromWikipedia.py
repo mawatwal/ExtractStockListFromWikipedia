0b2deb9d7f0d91538084b8322942bdc2c315567c
Manish Mawatwal <63574575+mawatwal@users.noreply.github.com>
2020-04-14 00:51:24 +0530

    Update ExtractStockListFromWikipedia.py

--- ExtractStockListFromWikipedia.py
+++ ExtractStockListFromWikipedia.py
@@ -1,5 +1,9 @@
+# import required libraries
 import pandas as pd
+# enter the URL of the Wikipedia page
 table = pd.read_html('https://en.wikipedia.org/wiki/List_of_BSE_SENSEX_companies')
+# extract the table
 df = table[0]
+# to create two different csv files 1.info 2.exchange tickers
 df.to_csv('Sensex-Info.csv')
 df.to_csv("Sensex-Tickers.csv", columns=['Exchange ticker'])
